5d318d0a9dee675f52179563fd7f87031b13e73f
vitalikx100 <vitalikstarigin@mail.ru>
2023-04-19 11:12:55 +0400

    Добавьте файлы проекта.

---
+++ Assembler_lab4/Assembler_lab4.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int A[5];
+	int sum = 0;
+	int b;
+	cout << "Enter 5 elements of the array: ";
+	for (int i = 0; i < 5; i++)
+	{
+		cin >> A[i];
+	}
+	cout << "Enter b: ";
+	cin >> b;
+
+
+	_asm
+	{
+		mov ecx, 5;
+		lea esi, A;
+		mov ebx, b;
+		xor eax, eax;
+
+	loop_start:
+		mov edx, [esi];
+		cmp edx, 0;
+		jle skip;
+
+		cmp edx, ebx;
+		jg skip;
+		add eax, edx;
+
+	skip:
+		add esi, 4;
+		dec ecx;
+		jnz loop_start;
+		mov sum, eax;
+	}
+	cout << sum;
+}
